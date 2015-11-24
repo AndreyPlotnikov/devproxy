4eb83e7912d0523c4011fbf68b92c0085c43cadb
plotnik <plotnikoff@gmail.com>
2015-11-24 23:57:08 +0300

    simple proxy

--- proxy.js
+++ proxy.js
@@ -1,3 +1,31 @@
-/**
- * Created by plotnik on 17.11.15.
- */
+var http = require('http');
+var urlLib = require('url');
+
+
+function handleRequest(request, response){
+    var parsedUrl = urlLib.parse(request.url);
+    var options = {
+        hostname: 'lenta.ru',
+        port: 80,
+        path: parsedUrl.href,
+        method: request.method
+    };
+    console.log(options);
+    var req = http.request(options, function(res){
+        console.log('response:', res.statusCode);
+        response.writeHead(res.statusCode, res.headers);
+        res.on('data', function(chunk){
+            response.write(chunk);
+        });
+        res.on('end', function(){
+            response.end();
+        });
+    });
+    req.on('error', function(err){
+        console.log('error:', err);
+    });
+    req.end();
+
+}
+
+http.createServer(handleRequest).listen(8080);
